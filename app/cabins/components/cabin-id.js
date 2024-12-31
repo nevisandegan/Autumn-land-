80928d93efde4e2450bd0d11e5f49ad2e3c4ac59
nevisandegan <nevisandehgan@gmail.com>
2024-12-31 10:44:42 +0330

    adding reservation form and data selector

--- app/cabins/components/cabin-id.js
+++ app/cabins/components/cabin-id.js
@@ -62,7 +62,7 @@ const CabinId = async ({ cabinId }) => {
 
       <div>
         <h2 className="text-5xl font-semibold text-center">
-          Reserve today. Pay on arrival.
+          Reserve {name} today. Pay on arrival.
         </h2>
       </div>
     </div>
