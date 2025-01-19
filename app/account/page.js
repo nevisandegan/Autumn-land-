9f705131d2c369b70021485b63ddde8c927844c1
nevisandegan <nevisandehgan@gmail.com>
2025-01-19 18:47:55 +0330

    implement middleware

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,17 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session =await auth();
+
+  console.log(session.user.name);
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Jonas
+      Welcome, {session.user.name}
     </h2>
   );
 }
